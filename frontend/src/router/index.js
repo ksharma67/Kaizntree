cdfd8186968cbf8d72d4fa87ec643a693ccb39b6
Kshitij Sharma <32421161+ksharma67@users.noreply.github.com>
2024-04-26 22:22:22 -0400

    Update index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AppView from '../views/AppView.vue'
 
+// Define the routes
 const routes = [
   {
     path: '/',
@@ -10,18 +11,21 @@ const routes = [
   {
     path: '/dasboard',
     name: 'dashboard',
+    // Use dynamic import to load the component lazily
     component: () => import(/* webpackChunkName: "about" */ '../views/UserDashboard.vue')
   },
   {
     path: '/items',
     name: 'Item Dashboard',
+    // Use dynamic import to load the component lazily
     component: () => import(/* webpackChunkName: "about" */ '../views/ItemDasboard.vue')
   }
 ]
 
+// Create the router instance
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  history: createWebHistory(process.env.BASE_URL), // Use the HTML5 history mode
+  routes // Pass the defined routes
 })
 
 export default router
